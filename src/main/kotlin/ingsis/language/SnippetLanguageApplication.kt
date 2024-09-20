3444606d7e62e3525e2e17488b8696510ffbcd43
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-09-20 13:28:03 -0300

    postgres db connected

---
+++ src/main/kotlin/ingsis/language/SnippetLanguageApplication.kt
@@ -0,0 +1,11 @@
+package ingsis.language
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetLanguageApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetLanguageApplication>(*args)
+}
---
+++ src/test/kotlin/ingsis/language/SnippetLanguageApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsis.language
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetLanguageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
