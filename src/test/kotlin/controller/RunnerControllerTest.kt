f07488086c1ed203bd700cc5b745f587455a09c3
Apetuch <alberto.pizzi@ing.austral.edu.ar>
2024-12-12 00:27:40 -0300

    test(Service,Controller):Runner controller to 80% cov

---
+++ src/test/kotlin/controller/RunnerControllerTest.kt
@@ -0,0 +1,87 @@
+package controller
+
+import ingsis.runner.runner.controller.RunnerController
+import ingsis.runner.runner.model.dto.ExecuteRequest
+import ingsis.runner.runner.model.dto.ExecutionResponse
+import ingsis.runner.runner.model.dto.SnippetRequest
+import ingsis.runner.runner.model.dto.format.FormatRequest
+import ingsis.runner.runner.model.dto.format.FormatResponse
+import ingsis.runner.runner.model.dto.lint.ValidationResponse
+import ingsis.runner.runner.service.RunnerService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.http.ResponseEntity
+import sca.StaticCodeAnalyzerError
+import java.security.Principal
+
+@ExtendWith(MockitoExtension::class)
+class RunnerControllerTest {
+    @Mock
+    private lateinit var runnerService: RunnerService
+
+    @Mock
+    private lateinit var principal: Principal
+
+    @InjectMocks
+    private lateinit var runnerController: RunnerController
+
+    private val authHeader = "Bearer token"
+
+
+
+    @Test
+    fun `execute snippet with empty content`() {
+        val executeRequest = ExecuteRequest("", "3.9")
+        val expectedResponse = ExecutionResponse(emptyList(), listOf("Error: No content to execute"))
+        whenever(runnerService.executeSnippet("", "3.9")).thenReturn(expectedResponse)
+
+        val response = runnerController.executeSnippet(executeRequest)
+
+        assertEquals(ResponseEntity.ok(expectedResponse), response)
+        verify(runnerService).executeSnippet("", "3.9")
+    }
+
+    @Test
+    fun `lint snippet with invalid language version`() {
+        whenever(principal.name).thenReturn("testUser")
+        val snippetRequest = SnippetRequest("test", "code", "python", "invalid_version")
+        val expectedResponse = ValidationResponse("test", false, "code", listOf(StaticCodeAnalyzerError("Error: Invalid language version")))
+        whenever(runnerService.lintSnippet("test", "code", "invalid_version", "testUser" , authHeader)).thenReturn(expectedResponse)
+
+        val response = runnerController.lintSnippet(snippetRequest, principal, authHeader)
+
+        assertEquals(ResponseEntity.ok(expectedResponse), response)
+        verify(runnerService).lintSnippet("test", "code", "invalid_version", "testUser", authHeader)
+    }
+
+    @Test
+    fun `execute snippet with valid content`() {
+        val executeRequest = ExecuteRequest("print('Hello, World!')", "3.9")
+        val expectedResponse = ExecutionResponse(listOf("Hello, World!"), emptyList())
+        whenever(runnerService.executeSnippet("print('Hello, World!')", "3.9")).thenReturn(expectedResponse)
+
+        val response = runnerController.executeSnippet(executeRequest)
+
+        assertEquals(ResponseEntity.ok(expectedResponse), response)
+        verify(runnerService).executeSnippet("print('Hello, World!')", "3.9")
+    }
+
+    @Test
+    fun `format snippet with valid content and version`() {
+        whenever(principal.name).thenReturn("testUser")
+        val formatRequest = FormatRequest("code", "3.9")
+        val expectedResponse = FormatResponse("formatted code")
+        whenever(runnerService.formatSnippet("code", "3.9", "testUser", authHeader)).thenReturn(expectedResponse)
+
+        val response = runnerController.formatSnippet(formatRequest, principal, authHeader)
+
+        assertEquals(ResponseEntity.ok(expectedResponse), response)
+        verify(runnerService).formatSnippet("code", "3.9", "testUser", authHeader)
+    }
+}
---
+++ src/test/kotlin/service/RunnerServiceTest.kt
@@ -0,0 +1,69 @@
+package service
+
+import ast.ASTNode
+import ingsis.runner.runner.model.dto.ExecutionResponse
+import ingsis.runner.runner.model.dto.RuleDTO
+import ingsis.runner.runner.model.dto.format.FormatResponse
+import ingsis.runner.runner.model.dto.lint.ValidationResponse
+import ingsis.runner.runner.model.enums.RuleTypeEnum
+import ingsis.runner.runner.redis.model.SnippetToValidate
+
+import ingsis.runner.runner.service.RunnerService
+import ingsis.runner.runner.service.common.FormatService
+import ingsis.runner.runner.service.common.InterpreterService
+import ingsis.runner.runner.service.common.ParserService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.web.client.RestTemplate
+
+class RunnerServiceTest {
+    @Mock
+    private lateinit var parserService: ParserService
+
+    @Mock
+    private lateinit var formatterService: FormatService
+
+    @Mock
+    private lateinit var interpreterService: InterpreterService
+
+    @Mock
+    private lateinit var restTemplate: RestTemplate
+
+    private val authHeader = "Bearer token"
+
+    private lateinit var runnerService: RunnerService
+
+    @BeforeEach
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        runnerService =
+            RunnerService(
+                parserService,
+                formatterService,
+                interpreterService,
+                restTemplate,
+            )
+    }
+
+    @Test
+    fun executeSnippet_invalidContent_throwsException() {
+        val content = "invalid content"
+        val version = "1.0"
+
+    whenever(parserService.parse(content, version)).thenThrow(RuntimeException("Parsing error"))
+
+    val exception = assertThrows<RuntimeException> {
+        runnerService.executeSnippet(content, version)
+    }
+
+    assertEquals("Parsing error", exception.message)
+    verify(parserService).parse(content, version)
+    }
+}
